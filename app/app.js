b77c7abb7eb12f65deb8c63e0ac41a9c096d6d73
Vanja Zeli <vanjazeli@gmail.com>
2022-01-07 19:57:35 +0100

    Added GSAP

--- app/app.js
+++ app/app.js
@@ -96,11 +96,209 @@ footerText.textContent = `© Fusely ${year}. All rights reserved.`;
 
 let body = document.querySelector("body");
 
-// document.addEventListener("onload", () => {
-// });
-
 window.onload = () => {
   textResponsiveness();
   swiperPosition();
   body.style.opacity = `1`;
 };
+
+// gsap
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".header__navbar", {
+  y: -110,
+  duration: 1,
+});
+
+gsap.from(".header__hero__primary-container__content", {
+  opacity: 0,
+  y: 150,
+  duration: 1.2,
+});
+
+gsap.from(".header__hero__primary-container__image", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+});
+
+gsap.from(".detailed__primary-container__image", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".detailed__primary-container__image",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".detailed__primary-container__content", {
+  opacity: 0,
+  y: 150,
+  duration: 1.2,
+  scrollTrigger: {
+    trigger: ".detailed__primary-container__content",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".creative__primary-container__image", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".creative__primary-container__image",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".creative__primary-container__content", {
+  opacity: 0,
+  y: 150,
+  duration: 1.2,
+  scrollTrigger: {
+    trigger: ".creative__primary-container__content",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".professional__primary-container__image", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".professional__primary-container__image",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".professional__primary-container__content", {
+  opacity: 0,
+  y: 150,
+  duration: 1.2,
+  scrollTrigger: {
+    trigger: ".professional__primary-container__content",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".choose__primary-container__heading", {
+  opacity: 0,
+  x: 100,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".choose__primary-container__heading",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".gsap-choose-first", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".choose__primary-container__content",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".gsap-choose-second", {
+  opacity: 0,
+  y: 150,
+  duration: 1.2,
+  scrollTrigger: {
+    trigger: ".choose__primary-container__content",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".gsap-choose-third", {
+  opacity: 0,
+  y: 200,
+  duration: 1.4,
+  scrollTrigger: {
+    trigger: ".choose__primary-container__content",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".customer__primary-container__heading", {
+  opacity: 0,
+  x: -100,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".customer__primary-container__heading",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".gsap-customer-first", {
+  opacity: 0,
+  x: 100,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".mySwiper",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".gsap-customer-second", {
+  opacity: 0,
+  x: 150,
+  duration: 1.2,
+  scrollTrigger: {
+    trigger: ".mySwiper",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".gsap-customer-third", {
+  opacity: 0,
+  x: 200,
+  duration: 1.4,
+  scrollTrigger: {
+    trigger: ".mySwiper",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".blog__primary-container__section-one__heading", {
+  opacity: 0,
+  x: 100,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".blog__primary-container__section-one__heading",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".gsap-blog-first", {
+  opacity: 0,
+  y: 200,
+  duration: 1.4,
+  scrollTrigger: {
+    trigger: ".gsap-blog-first",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".gsap-blog-second", {
+  opacity: 0,
+  y: 150,
+  duration: 1.2,
+  scrollTrigger: {
+    trigger: ".gsap-blog-second",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".gsap-blog-third", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".gsap-blog-third",
+    toggleActions: "play none none none",
+  },
+});
